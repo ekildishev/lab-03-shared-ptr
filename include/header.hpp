8208db12d2c903cf6682a6e3a10c19e343bbd182
ekildishev <47750057+ekildishev@users.noreply.github.com>
2019-11-04 16:03:11 +0300

    Update header.hpp

--- include/header.hpp
+++ include/header.hpp
@@ -1,6 +1,115 @@
 // Copyright 2018 Your Name <your_email>
-
-#ifndef INCLUDE_HEADER_HPP_
+#pragma once
+/*#ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
 
-#endif // INCLUDE_HEADER_HPP_
+#endif // INCLUDE_HEADER_HPP_*/
+
+#include <iostream>
+#include <cstddef>
+#include <atomic>
+
+template <typename T>
+class SharedPtr{
+    std::atomic_size_t *refCounter;
+    T* elemPointer = nullptr;
+public:
+    SharedPtr()
+    {
+        refCounter = new std::atomic_size_t(1);
+    }
+    SharedPtr(T* ptr)
+    {
+        elemPointer = ptr;
+        refCounter = new std::atomic_size_t(1);
+    }
+    SharedPtr(const SharedPtr& r)
+    {
+        *this = r;
+    }
+    SharedPtr(SharedPtr&& r)
+    {
+        *this = std::move(r);
+    }
+    ~SharedPtr()
+    {
+        if (!refCounter)
+        {
+            return;
+        }
+        if(*refCounter-- == 0)
+        {
+            delete refCounter;
+            delete elemPointer;
+        }
+    }
+    auto opeartor=(const SharedPtr& r) -> SharedPtr&
+    {
+        if(this == &r)
+        {
+            return *this;
+        }
+        this->~SharedPtr();
+        elemPointer = r.elemPointer;
+        refCounter = r.refCounter;
+        *(refCounter)++;
+        return *this;
+    }
+    auto opeartor=(const SharedPtr&& r) -> SharedPtr&
+    {
+        if(this == &r)
+        {
+            return *this;
+        }
+        this->~SharedPtr();
+        elemPointer = r.elemPointer;
+        refCounter = r.refCounter;
+        r.refCounter = nullptr;
+        r.elemPointer = nullptr;
+        *(refCounter)++;
+        return *this;
+    }
+
+    // проверяет, указывает ли указатель на объект
+    operator bool() const
+    {
+        if(elemPointer != nullptr)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    auto operator*() const -> T&
+    {
+        return *elemPointer;
+    }
+    auto operator->() const -> T*
+    {
+        return elemPointer;
+    }
+
+    auto get() -> T*
+    {
+        return elemPointer;
+    }
+    void reset()
+    {
+        *this = SharedPtr();
+    }
+    void reset(T* ptr)
+    {
+        *this = SharedPtr(ptr);
+    }
+    void swap(SharedPtr& r)
+    {
+        *this = SharedPtr(r);
+    }
+    // возвращает количество объектов SharedPtr, которые ссылаются на тот же управляемый объект
+    auto use_count() const -> size_t
+    {
+        return *refCounter;
+    }
+};
