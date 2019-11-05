352aca1a3607aa97b7af7d8ffae4820f8ed34fe5
ekildishev <47750057+ekildishev@users.noreply.github.com>
2019-11-05 10:03:38 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -26,6 +26,5 @@ TEST(Example, EmptyTest) {
     EXPECT_EQ(static_cast<bool>(intSharedPtrMove), true);
     EXPECT_EQ(intSharedPtrMove.operator->(), intNumPtr);
 
-    intSharedPtr.reset();
-    EXPECT_EQ(intSharedPtrMove.use_count(), 0);
+    intSharedPtrMove.reset();
 }
