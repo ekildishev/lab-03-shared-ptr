2fba7bd62150364d251187bc5c53fda72443ecb7
ekildishev <47750057+ekildishev@users.noreply.github.com>
2019-11-12 10:42:24 +0300

    Update coverage.sh

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -9,7 +9,7 @@ CMAKE_CONFIG_OPTS="-DHUNTER_CONFIGURATION_TYPES=Debug -DCMAKE_BUILD_TYPE=Debug"
 CMAKE_TOOLCHAIN_OPTS="-DCMAKE_TOOLCHAIN_FILE='`pwd`/tools/polly/gcc-7-cxx17-pic.cmake'"
 CMAKE_OPTS="$CMAKE_LINKER_OPTS $CMAKE_CONFIG_OPTS $CMAKE_TOOLCHAIN_OPTS"
 
-cmake -H. -B_builds $CMAKE_OPTS -DBUILD_COVERAGE=ON 
+cmake -H. -B_builds $CMAKE_OPTS -DBUILD_COVERAGE=ON
 cmake --build _builds
 cmake --build _builds --target test
 cmake --build _builds --target gcov
